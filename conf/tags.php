b454e986e9e0336f5be1a6d746308419e9f7a006
Israel Cidade <israelcbj@gmail.com>
2014-06-29 00:28:10 -0300

    [FIX] primeiro commit

---
+++ conf/tags.php
@@ -0,0 +1,21 @@
+<?php
+	#Definições do Sistema
+	date_default_timezone_set('America/Sao_Paulo');
+	define('UrlPadrao' , "http://localhost/presentenaweb/");
+	define('UrlDesenvolve',"presentenaweb");
+	
+	#Definições do Banco de Dados
+	define('DB_Host' , "localhost");
+	define('DB_Database' , "presentenaweb");
+	define('DB_User' , "root");
+	define('DB_Pass' , "");
+	
+	#Definições FPDF
+	define('DPI', 96);
+	define('MM_IN_INCH', 25.4);
+	define('A4_HEIGHT', 210);
+	define('A4_WIDTH', 297);
+	// tweak these values (in pixels)
+	define('MAX_WIDTH', 500);
+	define('MAX_HEIGHT', 500);
+?>
