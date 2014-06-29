4692daa196637252e33d40f84d032d5ab5d0d879
Israel Cidade <israelcbj@gmail.com>
2014-06-29 03:00:13 -0300

    arquivos

---
+++ functions/banco-cadastro.php
@@ -0,0 +1,5 @@
+<?php
+	class bancocadastro extends banco{
+		
+	}
+?>
---
+++ lib/cadastro.php
@@ -0,0 +1,10 @@
+<?php
+	#include das funcoes da tela inico
+	include('functions/banco-cadastro.php');
+
+	#Instancia o objeto
+	$banco = new bancocadastro();
+
+	#Imprimi valores
+	$Conteudo = $banco->CarregaHtml('cadastro');
+?>
