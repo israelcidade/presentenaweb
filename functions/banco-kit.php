eae7f80cafeee9832febc5de6f38645ba4b76d57
Israel Cidade <israelcbj@gmail.com>
2014-07-06 16:16:43 -0300

    [FIX] carrinho

--- functions/banco-kit.php
+++ functions/banco-kit.php
@@ -6,13 +6,20 @@
 			//inicia session sacola pra trabalhar com sacola
 			session_start('sacola');
 
+
+			//Zerar variavel Session !!!
+			//$_SESSION['sacola'] = '';
+			//die;
+
 			//contia script
 			$Sql = "Select * from c_kits where nome = '".$idkit."' ";
 			$result = $this->Execute($Sql);
 			$num_rows = $this->Linha($result);
 			
 			while($rs = mysql_fetch_array($result , MYSQL_ASSOC)){
-				$_SESSION['sacola'][] = $rs['idproduto'];
+				if(!in_array($rs['idproduto'], $_SESSION['sacola'])){
+					$_SESSION['sacola'][] = $rs['idproduto'];
+				}
 			}
 
 			return 'ok';
