4eabbf78ef313d43fbf006a81b0ffb418ca65b42
Israel Cidade <israelcbj@gmail.com>
2014-07-16 14:55:33 -0300

    [FIX] fazendo sacola

--- functions/banco-sacola.php
+++ functions/banco-sacola.php
@@ -2,34 +2,36 @@
 	class bancosacola extends banco{
 		
 		function ListaProdutosSacola($arr,$Auxilio){
-			$Banco_Vazio = "Banco esta Vazio";
+			if(!empty($arr)){
+				foreach ($arr as $value){
+					$Sql = "Select P.*, F.* from c_produtos P
+							INNER JOIN c_fotos F 
+							ON P.idproduto = F.idproduto
+							Where F.principal = '1'
+							And p.idproduto = '".$value."' ";
+					$result = parent::Execute($Sql);
+					$num_rows = parent::Linha($result);
+					$rs = mysql_fetch_array($result , MYSQL_ASSOC);
 
-			foreach ($arr as $value){
-				$Sql = "Select P.*, F.* from c_produtos P
-						INNER JOIN c_fotos F 
-						ON P.idproduto = F.idproduto
-						Where F.principal = '1'
-						And p.idproduto = '".$value."' ";
-				$result = parent::Execute($Sql);
-				$num_rows = parent::Linha($result);
-				$rs = mysql_fetch_array($result , MYSQL_ASSOC);
-
-				$Linha = $Auxilio;
-				$Linha = str_replace('<%ID%>',$rs['idproduto'],$Linha);
-				$Linha = str_replace('<%NOME%>',$rs['nome'],$Linha);
-				$Linha = str_replace('<%URLPADRAO%>',UrlPadrao,$Linha);
-				$Linha = str_replace('<%CAMINHO%>',$rs['caminho'],$Linha);
-				$Linha = str_replace('<%VALORVENDA%>',str_replace('.', ',',$rs['valorvenda']),$Linha);
-				$Produtos .= $Linha;
+					$Linha = $Auxilio;
+					$Linha = str_replace('<%ID%>',$rs['idproduto'],$Linha);
+					$Linha = str_replace('<%NOME%>',$rs['nome'],$Linha);
+					$Linha = str_replace('<%URLPADRAO%>',UrlPadrao,$Linha);
+					$Linha = str_replace('<%CAMINHO%>',$rs['caminho'],$Linha);
+					$Linha = str_replace('<%VALORVENDA%>',str_replace('.', ',',$rs['valorvenda']),$Linha);
+					$Produtos .= $Linha;
+				}
+			}else{
+				
 			}
-
 			return $Produtos;
 		}
 
 		function RemoveSacola($id){
 			session_start('sacola');
-			$key = array_search($id,$_SESSION['sacola']);
-			unset($_SESSION['sacola'][$key]);
+			//$key = array_search($id,$_SESSION['sacola']);
+			//unset($_SESSION['sacola'][$key]);
+			unset($_SESSION['sacola']);
 			return 'ok';
 		}
 
