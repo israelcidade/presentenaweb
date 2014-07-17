1d5a20910b7aa29aeae054cad7e2c9839469e359
Israel Cidade <israelcbj@gmail.com>
2014-07-17 00:21:03 -0300

    [FIX] fazendo o finalizar pedido

---
+++ html/style/js/mask.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('input[name=cep]').mask('00000-000');
+  $('input[name=telefone]').mask('(00) 0000-0000');
+  $('.date_time').mask('00/00/0000 00:00:00');
+  $('.cep').mask('00000-000');
+  $('.phone').mask('0000-0000');
+  $('.phone_with_ddd').mask('(00) 0000-0000');
+  $('.phone_us').mask('(000) 000-0000');
+  $('.mixed').mask('AAA 000-S0S');
+  $('.cpf').mask('000.000.000-00', {reverse: true});
+  $('.money').mask('000.000.000.000.000,00', {reverse: true});
+});
