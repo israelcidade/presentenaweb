59d4db6d7767241d53964b1219e4f74f711bfc54
Israel Cidade <israelcbj@gmail.com>
2014-07-17 13:50:51 -0300

    [FIX] fazendo ajax salva endereco

---
+++ lib/ajax/salvaendereco.php
@@ -0,0 +1,10 @@
+<?php
+	#cadastra email
+	include('../../functions/banco.php');
+	include('../../conf/tags.php');
+	$banco = new banco;
+	$banco->Conecta();
+	
+	//$email = $_POST["email"];
+	echo true;
+?>
