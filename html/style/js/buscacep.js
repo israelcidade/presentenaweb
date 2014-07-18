a4e377694412017264ed992d405c44411163f0e3
Israel Cidade <israelcbj@gmail.com>
2014-07-18 01:51:30 -0300

    [FIX] buscacep

--- html/style/js/buscacep.js
+++ html/style/js/buscacep.js
@@ -1,14 +1,21 @@
-function buscacep() {
-	if($.trim($("input[name=cep]").val()) != ""){
+function buscacep(){
+	var cep = $("input[name=cep]").val();
+	if(cep.length < 9 || cep == ''){
+		alert("Cep inválido");
+	}else{
 		$.getScript("http://cep.republicavirtual.com.br/web_cep.php?formato=javascript&cep="+$("input[name=cep]").val(), function(){
 		if(resultadoCEP["resultado"]){
-			$("input[name=endereco-entrega]").val(unescape(resultadoCEP["tipo_logradouro"])+": "+unescape(resultadoCEP["logradouro"]));
-			$("input[name=bairro]").val(unescape(resultadoCEP["bairro"]));
-			$("input[name=cidade]").val(unescape(resultadoCEP["cidade"]));
-			$("input[name=estado]").val(unescape(resultadoCEP["uf"]));
+			if(resultadoCEP["logradouro"] == ''){
+				alert("Endereço não encontrado");
+			}else{
+				$("input[name=endereco-entrega]").val(unescape(resultadoCEP["tipo_logradouro"])+": "+unescape(resultadoCEP["logradouro"]));
+				$("input[name=bairro]").val(unescape(resultadoCEP["bairro"]));
+				$("input[name=cidade]").val(unescape(resultadoCEP["cidade"]));
+				$("input[name=estado]").val(unescape(resultadoCEP["uf"]));
+			}
 		}else{
 			alert("Endereço não encontrado");
 			}
-		});				
+		});
 	}			
 }
