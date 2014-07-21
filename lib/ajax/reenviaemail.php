2786480025499a34908931c3ad66d4e9f03f7c7d
Israel Cidade <israelcbj@gmail.com>
2014-07-21 15:23:14 -0300

    [FIX] envio de nova senha

--- lib/ajax/reenviaemail.php
+++ lib/ajax/reenviaemail.php
@@ -12,46 +12,51 @@
 	
 	
 	if(preg_match($Syntaxe, $email)){
-		$novaSenha = $banco->GeraCodigo('10',false,false,false);
-		$Sql = "Update c_usuarios set senha = '".$novaSenha."' where email = '".$email."'";
+		$Sql = "Select * from c_usuarios where email = '".$email."'";
 		$result = $banco->Execute($Sql);
+		$num_rowns = $banco->Linha($result);
+		if($num_rowns){
+			$novaSenha = $banco->GeraCodigo('10',false,false,false);
+			$Sql = "Update c_usuarios set senha = '".$novaSenha."' where email = '".$email."'";
+			$result = $banco->Execute($Sql);
 
-		/*#Carrega classe MAILER
-		$mail = new PHPMailer();
-		// Charset para evitar erros de caracteres
-		$mail->Charset = 'UTF-8';
-		// Dados de quem está enviando o email
-		$mail->From = $email;
-		$mail->FromName = $nome;
+			#Carrega classe MAILER
+			$mail = new PHPMailer();
+			// Charset para evitar erros de caracteres
+			$mail->Charset = 'UTF-8';
+			// Dados de quem está enviando o email
+			$mail->From = 'contato@presentenaweb.com.br';
+			$mail->FromName = 'Presente na Web';
 
-		// Setando o conteudo
-		$mail->IsHTML(true);
-		$mail->Subject = "Contato Site Presente na Web";
-		$mail->Body = utf8_decode("
-			teste
-			");
-            
-        // Validando a autenticação
-		$mail->IsSMTP();
-		$mail->SMTPAuth = true;
-		$mail->Host     = "ssl://smtp.gmail.com";
-		$mail->Port     = 465;
-		$mail->Username = EMAIL_USER;
-		$mail->Password = EMAIL_PASS;
+			// Setando o conteudo
+			$mail->IsHTML(true);
+			$mail->Subject = "Nova Senha Site Presente Na Web";
+			$mail->Body = utf8_decode("
+				Nova Senha: $novaSenha
+				");
+	            
+	        // Validando a autenticação
+			$mail->IsSMTP();
+			$mail->SMTPAuth = true;
+			$mail->Host     = "ssl://smtp.gmail.com";
+			$mail->Port     = 465;
+			$mail->Username = EMAIL_USER;
+			$mail->Password = EMAIL_PASS;
 
-		// Setando o endereço de recebimento
-		$mail->AddAddress(EMAIL_RECEB);
-            
-		// Enviando o e-mail para o usuário
-        if($mail->Send()){
-        	echo 'ok';
-        }else{
-			echo 'false';
-        }
+			// Setando o endereço de recebimento
+			$mail->AddAddress($email);
+	            
+			// Enviando o e-mail para o usuário
+	        $enviado = $mail->Send();
+	        if($enviado){
+	        	echo 'enviado';
+	        }else{
+				echo 'false';
+	        }
+	    }else{
+	    	echo 'sememail';
+	    }
 	}else{
 		echo 'emailerrado';
-	}*/
-	echo 'ok';
 	}
-	
 ?>
