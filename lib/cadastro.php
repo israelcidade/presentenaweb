653b3c0e459447f5082a97176575279102a589d9
Israel Cidade <israelcbj@gmail.com>
2014-07-21 01:35:50 -0300

    [FIX] arrumando validacao

--- lib/cadastro.php
+++ lib/cadastro.php
@@ -18,7 +18,7 @@
 		}
 
 		if($this->PaginaAux[0] == "ativar"){
-			$msg = MSG_AVISO_ATIVAR;
+			$msg = $banco->MontaMsg('erro',MSG_AVISO_ATIVAR);
 		}
 
 		if($this->PaginaAux[0] == 'acesso-negado'){
