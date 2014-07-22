69d28157440b7743a957ada75581c0c25faaf733
Israel Cidade <israelcbj@gmail.com>
2014-07-22 02:47:37 -0300

    [FIX] pedido unico

--- functions/banco-pedido.php
+++ functions/banco-pedido.php
@@ -1,5 +1,40 @@
 <?php
 	class bancopedido extends banco{
-		
+		function VerificaPedido($idpedido,$idusuario){
+			$Sql = "Select * from c_pedidos where idpedido = '".$idpedido."' AND idusuario = '".$idusuario."'";
+			$result = parent::Execute($Sql);
+			$num_rowns = parent::Linha($result);
+
+			if($num_rowns){
+				return true;
+			}else{
+				return false;
+			}
+		}
+
+		function BuscaPedido($idpedido){
+			$Auxilio = parent::CarregaHtml('itens/pedido-itens');
+			$Sql = "Select P.*,R.*,K.nome as nomekit,S.nome as nomestatus from c_pedidos P 
+					INNER JOIN c_rastreios R ON P.idpedido = R.idpedido 
+					INNER JOIN c_kits K ON P.produtos = K.busca
+					INNER JOIN fixo_status S ON P.status = S.idstatus
+					Where P.idpedido = '".$idpedido."'
+					";
+					
+
+			$result = parent::Execute($Sql);
+			$num_rows = parent::Linha($result);
+			if($num_rows){
+				while($rs = mysql_fetch_array($result , MYSQL_ASSOC)){
+					$Linha = $Auxilio;
+					$Linha = str_replace('<%KIT%>',$rs['nomekit'],$Linha);
+					$Linha = str_replace('<%VALORTOTAL%>','R$ '.number_format($rs['total'], 2, ',', '.'),$Linha);
+					$Linha = str_replace('<%STATUS%>',$rs['nomestatus'],$Linha);
+					$Linha = str_replace('<%RASTREIO%>',$rs['codigo'],$Linha);
+					$Pedidos .= $Linha;
+				}
+			}
+			return $Pedidos;
+		}
 	}
 ?>
--- lib/pedido.php
+++ lib/pedido.php
@@ -7,11 +7,20 @@
 
     if($banco->VerificaSessao()){
 
-        
-        
-	    #Imprimi valores
-	    $Conteudo = $banco->CarregaHtml('pedido');
-	    $Conteudo = str_replace('<%URLPADRAO%>',UrlPadrao,$Conteudo);
+        $idpedido = $this->PaginaAux[0];
+        $result = $banco->VerificaPedido($idpedido,$_SESSION['idusuario']);
+        if($result == true){
+
+            $Pedido = $banco->BuscaPedido($idpedido);
+
+            #Imprimi valores
+            $Conteudo = $banco->CarregaHtml('pedido');
+            $Conteudo = str_replace('<%URLPADRAO%>',UrlPadrao,$Conteudo);
+            $Conteudo = str_replace('<%PEDIDO%>',$Pedido,$Conteudo);
+            $Conteudo = str_replace('<%IDPEDIDO%>',$idpedido,$Conteudo);
+        }else{
+            $banco->RedirecionaPara('cadastro/acesso-negado');
+        }
     }else{
 		$banco->RedirecionaPara('cadastro/acesso-negado');
 	}
