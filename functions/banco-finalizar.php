05224022fc1198c6eee28feb3d44925b2d0f7a45
Israel Cidade <israelcbj@gmail.com>
2014-07-23 18:00:17 -0300

    [FIX] finalizar pedido

--- functions/banco-finalizar.php
+++ functions/banco-finalizar.php
@@ -11,13 +11,16 @@
 				$result = $this->Execute($Sql);
 				$rs = mysql_fetch_array($result , MYSQL_ASSOC);
 
+				$total = ceil($rs['valorvenda']);
+				$total = number_format($total, 2, '.', '');
+
 				$Auxilio = parent::CarregaHtml('itens/pagseguro-itens');
 				
 				$Linha = $Auxilio;
 				$Linha = str_replace('<%I%>',$i+1,$Linha);
 				$Linha = str_replace('<%IDPRODUTO%>',$rs['idproduto'],$Linha);
 				$Linha = str_replace('<%NOME%>',$rs['nome'],$Linha);
-				$Linha = str_replace('<%VALOR%>',$rs['valorvenda'],$Linha);
+				$Linha = str_replace('<%VALOR%>',$total,$Linha);
 				$Linha = str_replace('<%QTD%>','1',$Linha);
 
 				$Produtos .= $Linha;
