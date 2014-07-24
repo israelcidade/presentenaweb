e300b345b55b8401fb0bc517dfc2bd98270523bf
Israel Cidade <israelcbj@gmail.com>
2014-07-24 16:19:10 -0300

    [FIX] finaliza

--- lib/minha-conta.php
+++ lib/minha-conta.php
@@ -7,6 +7,13 @@
 
 	if($banco->VerificaSessao()){
 
+		if($this->PaginaAux[0] == 'check'){
+			//$teste = 'http://localhost/presentenaweb/minha-conta/check/?id_pagseguro=F498A827-7870-42D7-B3A6-777F0A87241A';
+			$aux = explode('=',$this->PaginaAux[1]);
+			$codigo_pagseguro = $aux[1];
+		}	
+		
+
 		//Busca todas as compras realizadas
 		$Auxilio = $banco->CarregaHtml('itens/minha-conta-itens');
 
